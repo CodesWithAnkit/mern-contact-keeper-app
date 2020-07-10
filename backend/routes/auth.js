14a7309ddd19e770eb4c0a7ffc1152f323d72223
codeswithankit <ank8804465648@gmail.com>
2020-07-10 19:35:20 +0530

    authenticate route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,64 @@
 const express = require('express')
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+// validation
+const { check, validationResult } = require('express-validator')
+
+const User = require('../models/User')
+
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('Get Logged In user')
 })
 
-router.post('/', (req, res) => {
-  res.send('log In user')
-})
+router.post(
+  '/',
+  [
+    check('email', 'Please Include a vlaide Emalil').isEmail(),
+    check('password', 'password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { email, password } = req.body
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' })
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 36000000,
+        },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (error) {
+      console.error(err.message)
+      res.status(500).send('server Error')
+    }
+  }
+)
 
 module.exports = router
