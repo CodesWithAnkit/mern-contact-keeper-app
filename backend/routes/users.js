83a652afc705719aa5699636e10ddbf16160f379
codeswithankit <ank8804465648@gmail.com>
2020-07-10 18:49:18 +0530

    hash the password and save to db

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 // validation
 const { check, validationResult } = require('express-validator')
 
@@ -15,12 +16,32 @@ router.post(
       'Please provide a min length of 6 character password'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-    res.send('passed')
+
+    const { name, email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+      if (user) {
+        return res.status(400).json({ msg: 'User laready exists' })
+      }
+
+      user = new User({ name, email, password })
+      const salt = await bcrypt.genSalt(10)
+
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+      res.send('user saved')
+    } catch (error) {
+      console.log(error.message)
+      res.status(500).send('Server Error')
+    }
   }
 )
 
