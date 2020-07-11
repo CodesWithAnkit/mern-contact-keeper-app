737a3cad200522b70683d4122f7d5f6415b2b026
codeswithankit <ank8804465648@gmail.com>
2020-07-11 15:42:07 +0530

    contact form and add contact on ui

---
+++ backend/client/src/context/contact/contactReducer.js
@@ -0,0 +1,21 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  CLEAR_FILTER,
+  FILTER_CONTACTS,
+} from '../types'
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      }
+    default:
+      return state
+  }
+}
