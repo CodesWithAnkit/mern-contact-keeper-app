478b7417e0ff3d564b06d2077bb16bdad79c1e00
codeswithankit <ank8804465648@gmail.com>
2020-07-11 12:18:31 +0530

    basic structur of the app and navbar react-router-dom and more

---
+++ backend/client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">This is a full stact React app for keeping contact</p>
+      <p className="bg-dard p">
+        <strong>Version: </strong>1.0.1
+      </p>
+    </div>
+  )
+}
+
+export default About
