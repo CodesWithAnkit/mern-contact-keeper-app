eb6905670732e61bdefb98b7c0cffef96ec27c85
codeswithankit <ank8804465648@gmail.com>
2020-07-29 16:03:47 +0530

    ready for production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDb = require('./config/db')
+const path = require('path')
 
 const app = express()
 connectDb()
@@ -7,13 +8,20 @@ connectDb()
 // Init middleware dore req.body
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello wrold' })
-})
+app.get('/', (req, res) => {})
 
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/contacts', require('./routes/contacts'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
